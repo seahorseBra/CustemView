6c66e5b0967e3e5641ade0ae660b9172def05f97
seahorseBra <zchaoysu@163.com>
2017-09-24 10:37:23 +0800

    drawable给搞定

--- app/src/main/java/com/example/administrator/custemview/ImageDrawActivity.java
+++ app/src/main/java/com/example/administrator/custemview/ImageDrawActivity.java
@@ -1,23 +1,47 @@
 package com.example.administrator.custemview;
 
+import android.graphics.Bitmap;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RectShape;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 import java.util.ArrayList;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import utils.GlideApp;
 import view.ImageStackDrawable;
 
 public class ImageDrawActivity extends AppCompatActivity {
 
+    @Bind(R.id.img)
+    ImageView img;
+    @Bind(R.id.img1)
+    ImageView img1;
+    @Bind(R.id.img2)
+    ImageView img2;
+    @Bind(R.id.img3)
+    ImageView img3;
+    @Bind(R.id.img4)
+    ImageView img4;
+    @Bind(R.id.img5)
+    ImageView img5;
+    @Bind(R.id.img6)
+    ImageView img6;
     private ImageView mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_draw);
+        ButterKnife.bind(this);
+        ImageView[] is = {img1, img2, img3, img4, img5, img6};
         mImage = (ImageView) findViewById(R.id.img);
         ImageStackDrawable imageStackDrawable = new ImageStackDrawable(this);
         ArrayList<String> list = new ArrayList<>();
@@ -25,12 +49,26 @@ public class ImageDrawActivity extends AppCompatActivity {
         list.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1506079577514&di=d11b51459c2bb8528000b0427fbd28a8&imgtype=0&src=http%3A%2F%2Fimg0.ph.126.net%2FXjXl3KcowmXdE1pcsFVe8g%3D%3D%2F1067353111787095545.jpg");
         list.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1506079577514&di=61c7a9453105db85a9a6562340f83289&imgtype=0&src=http%3A%2F%2Fwww.laozhq.cn%2FUploadFile%2F2013-2%2F20132274451175515.jpg");
         list.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1506079577513&di=e9365b3711c87d4d09efa2bd1c003e99&imgtype=0&src=http%3A%2F%2Fimg.daimg.com%2Fuploads%2Fallimg%2F170214%2F3-1F214233558.jpg");
+        list.add("http://img.zcool.cn/community/05e5e1554af04100000115a8236351.jpg");
         list.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1506079577513&di=f943a6354adb4d12435067eba565d113&imgtype=0&src=http%3A%2F%2Fpic32.photophoto.cn%2F20140812%2F0035035784895932_b.jpg");
-        list.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1506079577509&di=138132430fff63d013e771cffe1e973f&imgtype=0&src=http%3A%2F%2Fd.image.i4.cn%2Fi4web%2Fimage%2Fupload%2F20170301%2F1488334069597013125.jpg");
         imageStackDrawable.setData(list);
         ShapeDrawable shapeDrawable = new ShapeDrawable(new RectShape());
         shapeDrawable.setIntrinsicHeight(100);
         shapeDrawable.setIntrinsicWidth(100);
         mImage.setImageDrawable(imageStackDrawable);
+
+
+
+        for (int i = 0; i < list.size(); i++) {
+            GlideApp.with(this)
+                    .asBitmap()
+                    .load(list.get(i))
+                    .placeholder(R.drawable.default_user_head_img)
+                    .error(R.drawable.default_user_head_img)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .skipMemoryCache(false)
+                    .into(is[i]);
+        }
+
     }
 }
--- app/src/main/java/view/ImageStackDrawable.java
+++ app/src/main/java/view/ImageStackDrawable.java
@@ -3,20 +3,29 @@ package view;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.RectF;
+import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.ViewUtils;
 
 
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.Request;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.target.SizeReadyCallback;
 import com.bumptech.glide.request.target.Target;
@@ -31,7 +40,7 @@ import utils.Utils;
 
 /**
  * Created by zchao on 2017/9/22.
- * desc:
+ * desc: Drawable的自定义过程。
  * version:
  */
 
@@ -41,10 +50,9 @@ public class ImageStackDrawable extends Drawable {
     ArrayList<String> list = new ArrayList<>();
     HashMap<String, Bitmap> mIconMap = new HashMap<>();
     private int mShowCompleteIconSize = 4;
-    private int mIconSize = (int) Utils.dp2Px(20);
-    private int mInterval = (int) Utils.dp2Px(5);
+    private int mIconSize = (int) Utils.dp2Px(50);
+    private int mInterval = (int) Utils.dp2Px(15);
     private RectF dstRect = new RectF();
-    private RectF srcRect = new RectF();
     private Bitmap defaultIcon = null;
 
     public ImageStackDrawable(Context context) {
@@ -67,72 +75,111 @@ public class ImageStackDrawable extends Drawable {
     private void loadIcon() {
         for (int i = 0; i < list.size(); i++) {
             final int finalI = i;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    handler.sendEmptyMessageDelayed(finalI, finalI * 1000);
+                }
+            }).start();
+
+        }
+    }
+
+    Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(final Message msg) {
+            super.handleMessage(msg);
             GlideApp.with(mContext)
                     .asBitmap()
-                    .load(list.get(i))
+                    .load(list.get(msg.what))
                     .placeholder(R.drawable.default_user_head_img)
                     .error(R.drawable.default_user_head_img)
                     .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .override(mIconSize, mIconSize)
+                    .centerCrop()
                     .skipMemoryCache(false)
                     .into(new SimpleTarget<Bitmap>() {
                         @Override
                         public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
                             if (resource != null && !resource.isRecycled()) {
-                                mIconMap.put(String.valueOf(finalI), resource);
+                                mIconMap.put(String.valueOf(msg.what), resource);
                                 invalidateSelf();
                             }
                         }
                     });
         }
-
-    }
+    };
 
     @Override
     public int getIntrinsicHeight() {
-        return getBounds().height();
+        return mIconSize;
     }
 
     @Override
     public int getIntrinsicWidth() {
-        return getBounds().width();
+        int height = mIconSize;
+        if (height > 0) {
+            mIconSize = height;
+        }
+        int width = 0;
+        if (list.size() >= 6) {
+            width = mIconSize * mShowCompleteIconSize + mInterval * (mShowCompleteIconSize + 2);
+        } else if (list.size() == 5) {
+            width = mIconSize * mShowCompleteIconSize + mInterval * (mShowCompleteIconSize + 1);
+        } else {
+            width = (mIconSize + mInterval) * list.size() - mInterval;
+        }
+        return width;
     }
 
     @Override
     public void draw(@NonNull Canvas canvas) {
         //先绘制被压着的两个
-        if (list.size() >= mShowCompleteIconSize + 2) {
-            Bitmap s = mIconMap.get(String.valueOf(mShowCompleteIconSize + 2));
-            if (s == null || s.isRecycled()) {
-                s = defaultIcon;
-            }
-            dstRect.set(getBounds().right - mIconSize, getBounds().top, getBounds().right, getBounds().bottom);
-            canvas.drawBitmap(s, null, dstRect, mPaint);
 
-            dstRect.offset(-mInterval, 0);
-            Bitmap s5 = mIconMap.get(String.valueOf(mShowCompleteIconSize + 1));
-            if (s5 == null || s5.isRecycled()) {
-                s5 = defaultIcon;
-            }
-            canvas.drawBitmap(s5, null, dstRect, mPaint);
-        } else if (list.size() == mShowCompleteIconSize + 1) {
+        dstRect.set(getBounds().left, getBounds().top, getBounds().left + mIconSize, getBounds().bottom);
+        if (list.size() >= mShowCompleteIconSize + 2) {
             Bitmap s = mIconMap.get(String.valueOf(mShowCompleteIconSize + 1));
             if (s == null || s.isRecycled()) {
                 s = defaultIcon;
             }
-            dstRect.set(getBounds().right - mIconSize, getBounds().top, getBounds().right, getBounds().bottom);
-            canvas.drawBitmap(s, null, dstRect, mPaint);
+            canvas.save();
+            canvas.translate(getBounds().width() - mIconSize, 0);
+            mPaint.setShader(new BitmapShader(s, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+            canvas.drawCircle(dstRect.centerX(), dstRect.centerY(), mIconSize / 2, mPaint);
+
+            dstRect.offset(-mInterval, 0);
+            Bitmap s5 = mIconMap.get(String.valueOf(mShowCompleteIconSize));
+            if (s5 == null || s5.isRecycled()) {
+                s5 = defaultIcon;
+            }
+            mPaint.setShader(new BitmapShader(s5, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+            canvas.drawCircle(dstRect.centerX(), dstRect.centerY(), mIconSize / 2, mPaint);
+            canvas.restore();
+        } else if (list.size() == mShowCompleteIconSize + 1) {
+            canvas.save();
+            canvas.translate(getBounds().width() - mIconSize, 0);
+            Bitmap s = mIconMap.get(String.valueOf(mShowCompleteIconSize));
+            if (s == null || s.isRecycled()) {
+                s = defaultIcon;
+            }
+            mPaint.setShader(new BitmapShader(s, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+            canvas.drawCircle(dstRect.centerX(), dstRect.centerY(), mIconSize / 2, mPaint);
+            canvas.restore();
         }
 
 
         dstRect.set(getBounds().left, getBounds().top, getBounds().left + mIconSize, getBounds().bottom);
+        canvas.save();
         for (int i = 0; i < Math.min(mShowCompleteIconSize, list.size()); i++) {
             Bitmap s = mIconMap.get(String.valueOf(i));
             if (s == null || s.isRecycled()) {
                 s = defaultIcon;
             }
-            canvas.drawBitmap(s, null, dstRect, mPaint);
-            dstRect.offset((mIconSize + mInterval), 0);
+            mPaint.setShader(new BitmapShader(s, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+            canvas.drawCircle(dstRect.centerX(), dstRect.centerY(), mIconSize / 2, mPaint);
+            canvas.translate((mIconSize + mInterval), 0);
         }
+        canvas.restore();
 
     }
 
